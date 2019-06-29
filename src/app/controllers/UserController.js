2d451fd89379f53e2010032b1a71797dc8640058
Rodrigo <rodrigoalves009@gmail.com>
2019-06-29 20:12:50 -0300

    finalizado o GoBarber do primeiro modulo

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation failure' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
@@ -14,7 +29,34 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation failure' });
+    }
+
     const { email, oldPassword } = req.body;
+
+    const usr = User.findOne({ where: { email } });
+
+    if (!usr) return res.status(400).json({ error: 'user not found' });
+
+    if (!oldPassword && !(await usr.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'password does not match' });
+    }
+
+    const { id, provider, name } = usr.update(req.body);
+
+    return res.send({ id, name, email, provider });
   }
 }
 
